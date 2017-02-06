7711bb3859fa28da2f3c0008e6faa610fd29b632
masonwolters <masonw919@gmail.com>
2017-02-06 15:19:24 -0500

    Index.js

--- index.js
+++ index.js
@@ -16,74 +16,11 @@ ghost = require('./core');
 errors = require('./core/server/errors');
 var PythonShell = require('python-shell');
 
-const path = require('path');
-var Nightmare = require('nightmare');
-require("nightmare-xpath");
-
 // Create our parent express app instance.
 parentApp = express();
 
 parentApp.get('/test-static', function(req, res) {
 	res.send('hi');
-});
-
-parentApp.get('/directory-remove', function(req, res) {
-	var secret = "ekHyXzbRqzsE3t3Ta3ri";
-	if (req.query.secret != secret) {
-		res.send('Not Authenticated');
-		return;
-	}
-	
-	var uniqnames = req.query.uniqnames.split(',');
-	
-	var nightmare = Nightmare({
-		webPreferences: {
-			preload: path.resolve('preload.js')
-		}
-	});
-
-	nightmare.goto('https://weblogin.umich.edu/?factors=UMICH.EDU&cosign-webdirectory.mc.itd&https://mcommunity.umich.edu/#group/members:Sailing%20Recruits')
-	  .wait('#login')
-	  .insert('#login', 'woltersm')
-	  .insert('#password', 'Dunedain9723')
-	  .click('#loginSubmit')
-	  .wait('#memberPeopleContent')
-	  .select('#memberPeopleContent select', '500')
-	  .wait(1)
-	  .evaluate(function(uniqs) {
-	  	success = [];
-	  	error = []
-
-	  	for (var i = 0; i < uniqs.length; i++) {
-	  		var uniqname = uniqs[i];
-	  		var node;
-		  	var xpathResult = document.evaluate("//div[@class='uniqname' and text()='"+uniqname+"']/../../../..//input[@type='checkbox']", document, null, XPathResult.ANY_TYPE, null);
-		  	node = xpathResult.iterateNext();
-		  	while (node && (node.name == 'owner' || node.name == 'moderator' || node.name == 'member')) {
-		  		node = xpathResult.iterateNext();
-		  	}
-		  	if (node) {
-		  		node.click();
-		  		success.push(uniqname);
-		  	} else {
-		  		//Doesn't exist
-		  		error.push(uniqname);
-		  	}
-	  	}
-
-	  	var button = document.getElementsByClassName('removeHolder')[0].childNodes[1].childNodes[0];
-	  	button.click();
-
-	  	return {
-	  		'success': success,
-	  		'error': error
-	  	}
-	  	
-	  }, uniqnames)
-	  .end()
-	  .then(function(result) {
-	  	res.send(JSON.stringify(result));
-	  });
 });
 
 ghost().then(function (ghostServer) {
